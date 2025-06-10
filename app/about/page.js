1ed15a7a4f84908ace1b2ee9d503ab208d27586b
ajay9679 <ajay.katlyn@gmail.com>
2025-06-10 15:54:08 +0530

    apostrophe s removed from about/page.js

--- app/about/page.js
+++ app/about/page.js
@@ -57,10 +57,10 @@ export default function Page() {
             dedication to creating a warm, welcoming environment.
           </p>
           <p>
-            Over the years, we've maintained the essence of The Wild Oasis,
+            Over the years, we haveve maintained the essence of The Wild Oasis,
             blending the timeless beauty of the mountains with the personal
-            touch only a family business can offer. Here, you're not just a
-            guest; you're part of our extended family. So join us at The Wild
+            touch only a family business can offer. Here, you arere not just a
+            guest; you are part of our extended family. So join us at The Wild
             Oasis soon, where tradition meets tranquility, and every visit is
             like coming home.
           </p>
